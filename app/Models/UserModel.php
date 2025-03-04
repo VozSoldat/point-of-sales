57857ba7e1665fb004b3294be36cf25b6489a2ce
VozSoldat <muhamaderril7@gmail.com>
2025-03-04 21:21:27 +0700

    praktikum-2.7
    relasi

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,4 +12,9 @@ class UserModel extends Model
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level()
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id');
+    }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -17,6 +17,8 @@
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <th>Kode Level</th>
+            <th>Nama Level</th>
             <td>Aksi</td>
         </tr>
         @foreach ($data as $d)
@@ -32,6 +34,12 @@
                 </td>
                 <td>
                     {{ $d->level_id }}
+                </td>
+                <td>
+                    {{ $d->level->level_kode }}
+                </td>
+                <td>
+                    {{ $d->level->level_nama }}
                 </td>
                 <td>
                     <a href="/user/ubah/{{$d->user_id}}">Ubah</a> | <a href="/user/hapus/{{$d->user_id}}">Hapus</a>
