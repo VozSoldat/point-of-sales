ebef7d70be31d441519211a33240fb9c5e8206ef
VozSoldat <muhamaderril7@gmail.com>
2025-03-10 09:43:11 +0700

    JB5/feat (view): create a welcome page

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,37 +20,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::prefix('category')->group(function () {
-    Route::get(
-        '/food-beverage',
-        [ProductController::class, 'foodBeverage']
-    );
-
-    Route::get(
-        '/beauty-health',
-        [ProductController::class, 'beautyHealth']
-    );
-    Route::get(
-        '/home-care',
-        [ProductController::class, 'homeCare']
-    );
-    Route::get(
-        '/baby-kid',
-        [ProductController::class, 'babyKid']
-    );
-});
-
-Route::get('/user/{id}/name/{name}',[UserController::class,'index'])->name('user');
-
-Route::get('/sales', [HomeController::class, 'sales'])->name('sales');
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('user.tambah_simpan');
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
